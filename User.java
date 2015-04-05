57b210083ea5faddf18d81227fc9af1f5fd8860c
AlexanderSiech <alex.siech@gmail.com>
2015-04-05 14:50:46 +0200

    User & Category
    la till 2 klasser User och Category lägg tiull det ni tycker behövs

---
+++ User.java
@@ -0,0 +1,63 @@
+public class User {
+	private String userName;
+	private String passWord;
+	private CategoryList categoryList;
+	private long ID;
+	private String firstName;
+	private String lastName;
+
+	public User(String userName, String passWord) {
+		this.userName = userName;
+		this.passWord = passWord;
+
+	}
+
+	public void setName(String UserName) {
+		this.userName = UserName;
+	}
+
+	public String getName() {
+		return userName;
+	}
+
+	public void setPassword(String passWord) {
+		this.passWord = passWord;
+	}
+
+	public String getPassword() {
+		return passWord;
+	}
+
+	public void setCategory(CategoryList categoryList) {
+		this.categoryList = categoryList;
+	}
+
+	public CategoryList getCategory() {
+		return categoryList;
+	}
+
+	public void setID(long ID) {
+		this.ID = ID;
+	}
+
+	public long getID() {
+		return ID;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+}
