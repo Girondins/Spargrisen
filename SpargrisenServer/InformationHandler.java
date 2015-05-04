71af7d5421034e4c4816181ffd29ca9ffaa8d13d
Girondins <stmsfsd@gmail.com>
2015-05-04 13:45:47 +0200

    Changes
    asfasf

--- SpargrisenServer/InformationHandler.java
+++ SpargrisenServer/InformationHandler.java
@@ -2,7 +2,9 @@ package SpargrisenServer;
 
 import java.io.IOException;
 
+import SpargrisenServer.Driver;
 import SpargrisenObjekt.Category;
+import SpargrisenObjekt.CategoryList;
 import SpargrisenObjekt.RegisterUser;
 import SpargrisenObjekt.AvailableUser;
 import SpargrisenObjekt.Tag;
@@ -10,54 +12,84 @@ import SpargrisenObjekt.Tag;
 public class InformationHandler {
 	private AvailableUser user;
 	
+	
+	//skapa en lista med category
 	public AvailableUser retriveUser(AvailableUser user) throws IOException {
 		String userName;
+		CategoryList catlist;
+		Category category;
 		userName = user.getName();
+		
+		Driver.retriveUser(userName);
+		
+		category = new category(driver)
+		
+		user.setCategoryList(catlist);
+	
+		
+		
 		
 		// JODY KOD med DATABAS HÄMTA VÄSENTLIG INFO FRÅN DATABAS OCH LAGRA I ETT USER OBJEKTET
 		// SOM SKICKAS TILLBAKA, ANVÄND SETMETODER
 		
 		return this.user;
 		
+		
 	}
-
+	
+	
+	
+	//DONE
 	public boolean doesUserExist(AvailableUser user) {
-		String userName, password;
+		String userName;
+		char[] password;
 		userName = user.getName();
 		password = user.getPassword();
 		
-		// JODY KOD med DATABAS KOLLA OCH JÄMNFÖR I DATABAS IFALL MATCH
-		// return True ifall stämmer annars return false
+		if(Driver.isUserFree(userName)==true){
+			System.out.println("User name available...");
+		}else{
+			System.out.println("user name taken...");
+		}
 		
-		
-		return true;
+		return Driver.isUserFree(userName);
 
 	}
-
+	//Not DONE, bryt ner char array till String viceversa
 	public AvailableUser registerUser(RegisterUser RegisterUser) throws IOException {
-		String userName, password;
+		String userName;
+		char[] password;
 		userName = RegisterUser.getName();
 		password = RegisterUser.getPassword();
 		user = new AvailableUser(userName);
-		
-		//JODY KOD med DATABAS AV SKAPANDE AV NY ANVÄNDAREN
-		
+		Driver.createNewUser(userName, password);
 		return user;
 
 	}
-	
+	//DONE
 	public AvailableUser addCategory(Category category){
-		String categoryName,userName;
+		String categoryName,userName, categoryID;
 		int limit;
 		categoryName = category.getCategoryName();
 		userName = category.getUser();
 		limit = category.getBudgetLimit(); //Budgetgräns
+		Driver.addCategory(categoryName, userName, limit);
 		
-		//JODY KOD MED DATABAS, SKAPA NY CATEGORY I TABELL
-		
-//		user.getCategoryList().addCategory(category);
 		return user;
+	}
+	//DONE
+	public AvailableUser addTag(Tag tag){
+		String tagName,categoryName,userName;
+		userName = tag.getUserName();
+		tagName = tag.getTagName();
+		if(tag.getCategoryName()==null){
+			categoryName = "Diverse";
+		}else{
+		categoryName = tag.getCategoryName();
+		}
+		Driver.addTag(userName, tagName, categoryName);
 		
+		return user;
 	}
 	
 	public AvailableUser changeBudgetLimit(Category category){
@@ -68,17 +100,6 @@ public class InformationHandler {
 		newLimit = category.getBudgetLimit();
 		
 		// JODY KOD MED DATABAS DÄR MAN ÄNDRAR BUDGETLIMIT
-		
-		return user;
-	}
-	
-	public AvailableUser addTag(Tag tag){
-		String tagName,categoryName,userName;
-		userName = tag.getUserName();
-		tagName = tag.getTagName();
-		categoryName = tag.getCategoryName();
-		
-		//JODY KOD MED DATABAS DÄR MAN LÄGGER TILL TAG I CATEGORY
 		
 		return user;
 	}
